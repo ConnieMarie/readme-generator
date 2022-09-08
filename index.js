bb5e9f52fa6a832dce9c53abe09ce5f4e2644ae6
ConnieMarie <constance.marie.strahan@gmail.com>
2022-09-07 19:05:54 -0500

    quesiton array setup

--- index.js
+++ index.js
@@ -1,7 +1,97 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username? (Required)',
+        validate: githubInput => {
+            if (githubInput) {
+              return true;
+            } else { 
+              console.log('Please enter your GitHub username!');
+              return false;
+            }
+          } 
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address? (Required)',
+        validate: emailInput => {
+            if (emailInput) {
+              return true;
+            } else { 
+              console.log('Please enter your email address!');
+              return false;
+            }
+          } 
+        
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+              return true;
+            } else { 
+              console.log('Please enter your project title!');
+              return false;
+            }
+          } 
+        
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project. (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+              return true;
+            } else { 
+              console.log('Please enter your project description!');
+              return false;
+            }
+          } 
+        
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide installation instructions, if any.'
+        
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide intructions on the usage of your application.'
+        
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide guidelines for contributing to your project.'
+        
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide instructions for testing your application.'
+        
+    },
+    {
+        type: 'list',
+        name: 'license',
+        choices: ['MIT', 'Apache', 'GLP', 'ISC', 'BSD', 'Mozilla'],
+        message: 'Select a license for your project.'
+        
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +101,5 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
